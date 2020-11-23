9ed9ebe884bf6d4c808d0db28ea414111c37cf88
Nahuel <nahuelsarrode@gmail.com>
2020-11-23 15:51:54 -0300

    heroku deploy with mongo atlas ok

--- dist/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    DB: {
-        URI: 'mongodb://localhost:27017/myapp'
-    }
-};
--- dist/specs/index.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai_1 = require("chai");
-xdescribe("index test", () => {
-    it("Should always pass", function () {
-        chai_1.expect(true).to.equal(true);
-    });
-});
