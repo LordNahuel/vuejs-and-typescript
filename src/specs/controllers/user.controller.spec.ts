34d69d0cba0dc1062df7f9148c778d1fab66e31e
Nahuel <nahuelsarrode@gmail.com>
2020-11-23 16:00:37 -0300

    collections from postman ok

--- src/specs/controllers/user.controller.spec.ts
+++ src/specs/controllers/user.controller.spec.ts
@@ -1,37 +1,10 @@
-import { doesNotMatch } from 'assert';
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import sinon from 'sinon'
 import * as userController from '../../controllers/user.controller';
 
-chai.should();
 // TODO: Search some form to validate the user with jwt in test. 
 describe('User Controller', () => {
     describe('signup method', () => {
-        const req = {
-            body: {
-                role: "user",
-                name: "someName",
-                username: "someUsername",
-                email: "someEmail",
-                password: "somePassword"
-            }
-        }
-
-        
-
-        beforeEach(function () {
-            const res = {
-                json: sinon.spy(),
-                status: sinon.stub().returns({ end: sinon.spy })
-            }    
-        })
-
-        afterEach(function () {
-            
-        })
-
-        it('Must retorn a token', () => {
-
-        })
+       
     })
 });
